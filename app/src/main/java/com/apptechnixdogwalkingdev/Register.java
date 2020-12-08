c86641eaf5453c795b594e572ea92a0b7d92566b
Samora Mabuya <mrsamoramabuya@gmail.com>
2020-12-08 09:03:53 +0200

    linking project

--- app/src/main/java/com/apptechnixdogwalkingdev/Register.java
+++ app/src/main/java/com/apptechnixdogwalkingdev/Register.java
@@ -1,16 +1,13 @@
 package com.apptechnixdogwalkingdev;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.register);
     }
 }
